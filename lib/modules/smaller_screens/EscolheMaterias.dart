66eef8f2fd141eceb2117f85b45ca6613188cf19
Bruno Pastre <pastre68@gmail.com>
2018-12-09 18:13:00 -0200

    Changed message

--- lib/modules/smaller_screens/EscolheMaterias.dart
+++ lib/modules/smaller_screens/EscolheMaterias.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:horariopucpr/modules/io/Api.dart';
 import 'package:horariopucpr/modules/io/Storage.dart';
+import 'package:horariopucpr/modules/smaller_screens/LoadingScreen.dart';
 import 'package:horariopucpr/modules/utils/Utils.dart';
 
 class Picker extends StatefulWidget {
@@ -42,7 +43,7 @@ class PickerState extends State<Picker> {
             print('Pressed info!');
             showModalBottomSheet(context: this.context,builder: (BuildContext context){
               return Text('Estamos tentando montar a sua grade automaticamente\n'+
-              'Qualquer conflito será mostrado nesta página, e pedimos que você escolha a matéria certa\n'+
+              'Qualquer dúvida que o sistema tenha será mostrada nesta página, e pedimos que você escolha a matéria certa\n'+
               'Esse processo só precisa ser feito uma vez', );
             });
           }, icon: Icon(Icons.info_outline),)
@@ -53,7 +54,7 @@ class PickerState extends State<Picker> {
 
   int startTime(i) {
     var tmp = i['starttime'].split(':');
-//    print('Tmp is $tmp');
+//    print('Tmp is $tmp');py
     var ret = int.parse(tmp[0]) * 100 + int.parse(tmp[1]);
     print('Ret is $ret');
     return ret;
@@ -103,7 +104,7 @@ class PickerState extends State<Picker> {
   }
 
   Widget buildScreen(BuildContext context) {
-    if (resp.isEmpty) return Text('Loading');
+    if (resp.isEmpty) return LoadingWidget();
     List<Conflito> conflitos = buildConflitos();
     List<ListTile> tiles = new List<ListTile>();
     for (Conflito c in conflitos)
